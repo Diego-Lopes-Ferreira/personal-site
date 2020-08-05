212a5de72c04ad2d8354c4ac7c06756dc0fcbfdf
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-08-05 08:31:20 -0300

    Starter template

---
+++ src/pages/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+export default function Header() {
+  return (
+    <header className="app-header-wrapper">
+      <div className="app-header">
+
+      </div>
+    </header>
+  );
+}
