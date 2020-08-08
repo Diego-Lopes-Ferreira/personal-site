3d7b668e6c06ae26b8d8224d31635df566817da6
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-08-08 11:53:00 -0300

    Skill set implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './pages/header';
 import Home from './pages/home';
 import About from './pages/about';
+import Skills from './pages/Skills';
 
 //const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
 function scrollToRef(ref) {
@@ -13,11 +14,13 @@ function scrollToRef(ref) {
 export default function App() {
   const homeRef = useRef(null);
   const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
   return (
     <div className="App">
       {/* <Header /> */}
       <Home refere={homeRef} scrollTo={() => scrollToRef(aboutRef)} />
       <About refere={aboutRef} />
+      <Skills refere={skillsRef} />
     </div>
   );
 }
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,18 +1,57 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 
-
 export default function Skills({ refere }) {
+  const [hardSkill, setWhatSkill] = useState(true);
+  const [skillList, setSkillList] = useState(personInfo.hardSkills);
+  const [skills, setSkills] = useState();
+
+  useEffect(() => {
+    if (hardSkill) {
+      /* setSkillList(personInfo.hardSkills) */
+      setSkills( personInfo.hardSkills.map((item) => (
+        <ProgressBar title={item.title} progress={item.percentage} />
+      ))
+      )
+    } else {
+      /* setSkillList(personInfo.softSkills) */
+      setSkills(personInfo.softSkills.map((item) => (
+        <ProgressBar title={item.title} progress={item.percentage} />
+      ))
+      )
+    }
+  }, [hardSkill])
+
+  const notSelected = {
+    borderBottomColor: '#414141',
+  }
+  const selected = {
+    borderTopColor: '#414141',
+    borderRightColor: '#414141',
+    borderLeftColor: '#414141',
+    borderBottomColor: '#00000000',
+  }
+
+
+
   return (
-    <div ref={refere} className="app-about-skills-wrapper">
-      <div className="app-about-skills">
+    <div ref={refere} className="app-skills-wrapper">
+      <div className="app-skills">
         <nav>
-          <a href=""></a>
-          <a href=""></a>
+          <button
+            style={hardSkill ? selected : notSelected}
+            onClick={() => setWhatSkill(true)}
+          >Hard Skills</button>
+          <button
+            style={hardSkill ? notSelected : selected}
+            onClick={() => setWhatSkill(false)}
+          >Soft Skills</button>
         </nav>
-        <div className="sla">
-          <ProgressBar />
+
+        <div className="app-skill-progress">
+          {skills}
         </div>
+
       </div>
     </div>
   );
@@ -20,13 +59,36 @@ export default function Skills({ refere }) {
 
 function ProgressBar({ title, progress }) {
   return (
-    <div>
-      <div>
+    <div className="app-skill-progress-wrapper">
+      <div className="app-skill-progress-txt">
         <p>{title}</p>
       </div>
-      <div className="">
-        <div className="" style={{ width: `${progress}%` }}></div>
+      <div className="app-skill-bar">
+        <div className="app-skill-bar-progress" style={{ backgroundColor: '#414141', width: `${progress}%` }}></div>
       </div>
     </div>
   );
+}
+
+const personInfo = {
+  softSkills: [
+    { title: 'SOFT', percentage: '40' },
+    { title: 'SOFT', percentage: '40' },
+    { title: 'SOFT', percentage: '40' },
+    { title: 'SOFT', percentage: '40' },
+    { title: 'SOFT', percentage: '40' },
+    { title: 'SOFT', percentage: '40' },
+    { title: 'SOFT', percentage: '40' },
+    { title: 'SOFT', percentage: '40' },
+  ],
+  hardSkills: [
+    { title: 'HARD', percentage: '80' },
+    { title: 'HARD', percentage: '80' },
+    { title: 'HARD', percentage: '80' },
+    { title: 'HARD', percentage: '80' },
+    { title: 'HARD', percentage: '80' },
+    { title: 'HARD', percentage: '80' },
+    { title: 'HARD', percentage: '80' },
+    { title: 'HARD', percentage: '80' },
+  ],
 }
