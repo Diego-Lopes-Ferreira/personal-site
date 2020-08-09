2c402096c15b400b554dfb7d42eb5eb4c2439c4f
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-08-09 14:15:45 -0300

    Implemented header color change

--- src/pages/header.js
+++ src/pages/header.js
@@ -3,12 +3,13 @@ import '../App.css';
 
 export default function Header({ functions }) {
   const [bgColor, setBgColor] = useState('#00000000');
+  const [txtColor, settxtColor] = useState('#00000000');
 
   const [scrollPosition, setSrollPosition] = useState(0);
   const handleScroll = () => {
     const position = window.pageYOffset;
     setSrollPosition(position);
-    
+
   };
 
   useEffect(() => {
@@ -18,24 +19,26 @@ export default function Header({ functions }) {
     };
   }, []);
   useEffect(() => {
-    if(scrollPosition > 400) {
-      setBgColor('linear-gradient(#414141, #414141aa, #41414100) ');
+    if (scrollPosition > 400) {
+      setBgColor('#dddddd');
+      settxtColor('#414141')
     } else {
-      setBgColor('linear-gradient(#41414100, #41414100');
+      setBgColor('#00000000');
+      settxtColor('#dddddd');
     }
   }, [scrollPosition])
 
   return (
-    <header className="app-header-wrapper" style={{ backgroundColor: '#00000000', backgroundImage: bgColor }}>
+    <header className="app-header-wrapper" style={{ backgroundColor: bgColor }}>
       <div className="app-header">
         <div className="app-header-name">
-          <p>{personInfo.name}</p>
+          <p style={{ color: txtColor }}>{personInfo.name}</p>
         </div>
         <div className="app-header-pages">
-          <button onClick={() => { functions.goToHome() }} >Home</button>
-          <button onClick={() => { functions.goToAbout() }} >Sobre</button>
-          <button onClick={() => { functions.goToSkills() }} >Skills</button>
-          <button onClick={() => { functions.goToBlog() }} >Blog</button>
+          <button style={{ color: txtColor }} onClick={() => { functions.goToHome() }} >Home</button>
+          <button style={{ color: txtColor }} onClick={() => { functions.goToAbout() }} >Sobre</button>
+          <button style={{ color: txtColor }} onClick={() => { functions.goToSkills() }} >Skills</button>
+          <button style={{ color: txtColor }} onClick={() => { functions.goToBlog() }} >Blog</button>
         </div>
       </div>
     </header>
