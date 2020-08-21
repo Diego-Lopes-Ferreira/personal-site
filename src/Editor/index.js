aa48a8589c4e05cdfebd4908ae9f19da3a1a9fa2
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-08-21 14:15:28 -0300

    feat(editor): add ReactQuill

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import './editor.css';
 
-import TextBlockEditable from './components/TextBlockEditable';
-
 export default function TextBlockEditor({ initialData, configuration, onSave }) {
+  const [value, setValue] = useState('');
   return (
-    <div className="text-block-editor">
+    <div className="editor-page">
       <h1>DEEGO</h1>
-      <TextBlockEditable />
+      <div className="editor-wrapper">
+        <ReactQuill theme="snow" value={value} onChange={setValue} />
+      </div>
     </div>
   );
 }
