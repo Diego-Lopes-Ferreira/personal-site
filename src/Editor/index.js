1097839798067ed3cf775d6f4dc152c5a763691e
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-08-21 15:03:53 -0300

    feat(editor): implemented ReactQuill on the page

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -9,8 +9,60 @@ export default function TextBlockEditor({ initialData, configuration, onSave }) 
     <div className="editor-page">
       <h1>DEEGO</h1>
       <div className="editor-wrapper">
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        <ReactQuill
+          theme="snow"
+          modules={{ toolbar: myToolbarOptions }}
+          formats={myFormats}
+          value={value}
+          onChange={setValue} />
       </div>
     </div>
   );
 }
+
+// quill configuration  
+var myFormats = [
+  'background',
+  'bold',
+  'color',
+  'font',
+  'code',
+  'italic',
+  'link',
+  'size',
+  'strike',
+  'script',
+  'underline',
+  'blockquote',
+  'header',
+  'indent',
+  'list',
+  'align',
+  'direction',
+  'code-block',
+  'Embeds',
+  'formula',
+  'image',
+  'video',
+];
+
+var myToolbarOptions = [
+  [{ 'header': [1, 2, 3, false] }],
+  //[{ 'header': 1 }, { 'header': 2 }],
+  ['bold', 'italic', 'underline', 'strike', { 'align': ['', 'right', 'center', 'justify'] }],
+  [{ 'color': [] }, { 'background': [] }],
+  [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+  [
+    { 'script': 'sub' },
+    { 'script': 'super' }, // superscript/subscript
+    { 'indent': '-1' },
+    { 'indent': '+1' }
+  ], // outdent/indent
+  //[{ 'direction': 'rtl' }], // text direction
+  //[{ 'size': ['small', false, 'large', 'huge'] }],
+
+  ['image', 'link', 'video', 'blockquote', 'code-block'],
+  [{ 'font': [] }],
+  ['clean']
+];
+
